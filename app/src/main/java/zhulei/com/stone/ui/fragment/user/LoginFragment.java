6b0e44042edd771621f7f61d6cb1d6f63639839e
Zzz468005600 <zhulei199010@yahoo.com>
2018-07-27 21:55:16 +0800

    .

--- app/src/main/java/zhulei/com/stone/ui/fragment/user/LoginFragment.java
+++ app/src/main/java/zhulei/com/stone/ui/fragment/user/LoginFragment.java
@@ -3,6 +3,11 @@ package zhulei.com.stone.ui.fragment.user;
 import zhulei.com.stone.R;
 import zhulei.com.stone.ui.base.BaseFragment;
 
+/**
+ * author: zhulei
+ * date: 2016/7/26
+ * blog: http://www.zhuleiblog.com/
+ */
 public class LoginFragment extends BaseFragment {
 
     public static LoginFragment newInstance(){
